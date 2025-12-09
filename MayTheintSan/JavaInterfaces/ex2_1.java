dee446e15d006ef32f8ff793340451fb29161001
May Theint San <maythaiksan@gmail.com>
2025-12-09 16:58:14 +0630

    Create ex2_1.java

---
+++ MayTheintSan/JavaInterfaces/ex2_1.java
@@ -0,0 +1,10 @@
+package java_interfaces;
+
+
+public interface Account {
+ // Interface methods (all are public and abstract by default)
+ void deposit(double amount);
+ void withdraw(double amount);
+ void calculateInterest();
+ void viewBalance();
+}
