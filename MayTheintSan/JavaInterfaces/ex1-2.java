2f5a5f933d8994a916f8b9f997a7c13f430386d5
May Theint San <maythaiksan@gmail.com>
2025-12-09 16:43:25 +0630

    Create ex1-2.java

---
+++ MayTheintSan/JavaInterfaces/ex1-2.java
@@ -0,0 +1,9 @@
+package java_interfaces;
+
+//File: Spacecraft.java
+public class SpaceCraft implements Flyable {
+ @Override
+ public void fly_obj() {
+     System.out.println("Spacecraft is flying out of Earth's atmosphere.");
+ }
+}
