d5d484aaad3885567298ff89372bce2a7e2eba97
May Theint San <maythaiksan@gmail.com>
2025-12-11 09:14:34 +0630

    Create Helicopter.java

---
+++ May-Java2HWexercises/Java_Interface/Helicopter.java
@@ -0,0 +1,8 @@
+package java_interfaces;
+
+public class Helicopter implements Flyable {
+ @Override
+ public void fly_obj() {
+     System.out.println("Helicopter is taking off vertically and hovering.");
+ }
+}
