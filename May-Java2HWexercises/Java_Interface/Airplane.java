a6da751738ec23e155a392bd20080f98dc1675c7
May Theint San <maythaiksan@gmail.com>
2025-12-11 09:12:28 +0630

    Create Airplane.java

---
+++ May-Java2HWexercises/Java_Interface/Airplane.java
@@ -0,0 +1,9 @@
+package java_interfaces;
+
+
+public class Airplane implements Flyable {
+ @Override
+ public void fly_obj() {
+     System.out.println("Airplane is flying high in the troposphere.");
+ }
+}
