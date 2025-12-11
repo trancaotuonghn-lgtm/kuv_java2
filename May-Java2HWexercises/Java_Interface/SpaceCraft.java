09e6d553bcd8300cf73308a2c2ba4d2836793a4f
May Theint San <maythaiksan@gmail.com>
2025-12-11 09:16:35 +0630

    Create SpaceCraft.java

---
+++ May-Java2HWexercises/Java_Interface/SpaceCraft.java
@@ -0,0 +1,9 @@
+package java_interfaces;
+
+
+public class SpaceCraft implements Flyable {
+ @Override
+ public void fly_obj() {
+     System.out.println("Spacecraft is flying out of Earth's atmosphere.");
+ }
+}
